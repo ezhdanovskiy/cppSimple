137a9a16c1663c2ce3ee7e836c7103672b197869
Evgeny Zhdanovskiy <7323@list.ru>
2016-01-10 19:36:40 +0300

    add operations.h

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "operations.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "3 + 2 = " << sum(3, 2) << endl;
+	cout << "3 - 2 = " << minus(3, 2) << endl;
+	return 0;
+}
---
+++ src/operations.cpp
@@ -0,0 +1,9 @@
+#include "operations.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int minus(int a, int b) {
+    return a - b;
+}
